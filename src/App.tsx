0869ae5b56e276c98eeab817843198afab6a75c1
Toloza Laureano <laublake213@gmail.com>
2025-09-19 14:55:37 -0300

    Co-authored-by: Alejo Garrido <GarridoAlejo@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import Header from  './core/templates/Header'
 import './index.css'
 import Body from './core/templates/Body'
 import {BoxProvider} from './core/context/misteryBoxContext'
+import ClinicaLogin from './features/users/components/login';
 
 function App() {
   return (
-    <BoxProvider>
-      <Header clinicName="Portal Clinica Sana" />
-      <Body />      
-    </BoxProvider>
+    <ClinicaLogin/>
   )
 }
 
--- src/core/templates/Body.tsx
+++ src/core/templates/Body.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import './Body.css'
 import Container from '../components/container'
 import { motion } from 'framer-motion';
