5ab322b1faf52e797eb4a60f382093259ca080c8
AgustinAlfieri <agustinalfieri98@gmail.com>
2025-10-08 19:25:50 -0300

    Update routing for '/' path in AppRouter and add logo click navigation in NavBar

--- src/Router/AppRouter.tsx
+++ src/Router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ClinicaLogin from '../features/users/components/login'
 import AppointmentForm from '../features/appointment/components/appointment'
 import RegisterMedic from '../features/users/medic/components/RegisterMedic'
@@ -10,7 +10,7 @@ const AppRouter = () => {
     return(
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Hola mundo</h1>} />
+        <Route path="/" element={<Navigate to="/homepage" replace />} />
         <Route path="/login" element= {<ClinicaLogin />} />
         <Route path="/appointment" element= {<AppointmentForm />} />
         <Route path="/register" element= {<RegisterPatient/>} />
--- src/features/homepage/components/navBar.tsx
+++ src/features/homepage/components/navBar.tsx
@@ -1,10 +1,20 @@
 import './navBar.css'
 import logo from '../../../assets/medivia1.jpg';
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 const NavBar = () => {
+    
+    const navigate = useNavigate();
+    function handleLogoClick(){
+        navigate('/homepage');
+    }
+
     return (
         <header className="header">
-            <img src={logo} alt="Logo" className="logo" />
+            <img src={logo} alt="Logo" className="logo" style={{ cursor: 'pointer' }} onClick={handleLogoClick} />
             <nav className="navbar">
                 <div>
                     <a href="/login">Iniciar Sesión</a>
