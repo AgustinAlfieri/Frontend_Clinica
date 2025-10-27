c55ec20536f6cf095290134142a59e3c27165fd9
Agustín Alfieri <90259589+AgustinAlfieri@users.noreply.github.com>
2025-10-27 15:42:53 -0300

    Traigo la URL de la API desde el .env

--- src/features/users/services/api.ts
+++ src/features/users/services/api.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:3000/app/v1';
+//const API_BASE_URL = 'http://localhost:3000/app/v1';
+//Traigo la URL de la API desde el .env
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const apiClient = {
   async post(endpoint: string, data: any) {
